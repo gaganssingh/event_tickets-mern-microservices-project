d77bc85c307058913a606967d7c6f2b490947815
gaganssingh <gagansingh.dev@outlook.com>
2022-03-13 17:22:36 -0400

    Refactored current user check logic to a middleware

--- auth/src/routes/current-user.router.ts
+++ auth/src/routes/current-user.router.ts
@@ -1,25 +1,12 @@
 import express, { Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const currentUserRouter = express.Router();
 
-currentUserRouter.route("/currentuser").get((req: Request, res: Response) => {
-  console.log("req.session.jwt", req.session?.jwt);
-  // Check if the the user had previously signed
-  // We used cookie-session package to add the session obj with current user's info
-  // to the request body
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  // If the session object is present on the req
-  // verify the jwt token
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    return res.send({ currentUser: null });
-  }
-});
+currentUserRouter
+  .route("/currentuser")
+  .get(currentUser, (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
+  });
 
 export { currentUserRouter };
