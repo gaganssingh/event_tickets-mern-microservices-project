d83ef3e9db940b960a8a8c9ebb26e7fd2db58afc
gaganssingh <gagansingh.dev@outlook.com>
2022-03-13 17:04:58 -0400

    Implemented signout functionality

--- auth/src/routes/signout.router.ts
+++ auth/src/routes/signout.router.ts
@@ -2,6 +2,10 @@ import express, { Request, Response } from "express";
 
 const signoutRouter = express.Router();
 
-signoutRouter.route("/signout").post((req: Request, res: Response) => {});
+signoutRouter.route("/signout").post((req: Request, res: Response) => {
+  req.session = null;
+
+  return res.send({ message: "Signed out successfully" });
+});
 
 export { signoutRouter };
