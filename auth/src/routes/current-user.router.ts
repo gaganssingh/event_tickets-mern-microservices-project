87f97850bd34f85b7b9170d47eb33beeb17eb655
gaganssingh <gagansingh.dev@outlook.com>
2022-03-13 17:00:39 -0400

    Implemented the current user route

--- auth/src/routes/current-user.router.ts
+++ auth/src/routes/current-user.router.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const currentUserRouter = express.Router();
 
 currentUserRouter.route("/currentuser").get((req: Request, res: Response) => {
-  res.json({ message: "😊 Hello from express" });
+  console.log("req.session.jwt", req.session?.jwt);
+  // Check if the the user had previously signed
+  // We used cookie-session package to add the session obj with current user's info
+  // to the request body
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  // If the session object is present on the req
+  // verify the jwt token
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { currentUserRouter };
