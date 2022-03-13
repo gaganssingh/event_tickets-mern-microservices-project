4fae7cb28c5f226defafa6d13450e70e5ee5cfab
gaganssingh <gagansingh.dev@outlook.com>
2022-03-13 17:35:33 -0400

    Implemented a middleware to check is the user is signed in or not

--- auth/src/routes/current-user.router.ts
+++ auth/src/routes/current-user.router.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from "express";
 import { currentUser } from "../middlewares/current-user";
-import { requireAuth } from "../middlewares/require-auth";
 
 const currentUserRouter = express.Router();
 
 currentUserRouter
   .route("/currentuser")
-  .get(currentUser, requireAuth, (req: Request, res: Response) => {
+  .get(currentUser, (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null });
   });
 
