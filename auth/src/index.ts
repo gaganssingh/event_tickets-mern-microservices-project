c1745afc976ff33bf6ed4251883f4571c5b8a417
Gagan Singh <gagansingh.dev@outlook.com>
2022-03-15 22:11:51 -0400

    Updated mongodb connection urls to use env vars

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,8 +6,11 @@ import { app } from "./app";
   if (!process.env.JWT_KEY) {
     throw new Error(`❌ JWT_KEY must be defined`);
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error(`❌ JWT_KEY must be defined`);
+  }
   try {
-    await mongoose.connect(`mongodb://auth-mongo-srv:27017/auth`);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log(`[✔ AUTH DATABASE] Successfully connected`);
   } catch (err) {
     console.error(err);
